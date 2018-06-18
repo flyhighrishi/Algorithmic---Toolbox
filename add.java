4a6b6717a4ed06d422d0c2346727f04032977d7d
Ambitiou Idiot <rishikeshshukla47@gmail.com>
2018-06-18 20:53:26 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class AddNumbers
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+        Scanner in = new Scanner(System.in);
+		x = in.nextInt();
+		y = in.nextInt();
+		System.out.println(x+y);
+   }
+}
